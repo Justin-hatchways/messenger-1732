94002ec4499b7b60e9102422c884c42a4fdccf5b
Justin Gaudet <gaudet@gmail.com>
2021-10-12 19:03:10 -0300

    fix addNewConvoToStore

--- client/src/store/utils/reducerFunctions.js
+++ client/src/store/utils/reducerFunctions.js
@@ -69,10 +69,9 @@ export const addSearchedUsersToStore = (state, users) => {
 export const addNewConvoToStore = (state, recipientId, message) => {
   return state.map((convo) => {
     if (convo.otherUser.id === recipientId) {
-      convo.id = message.conversationId;
-      convo.messages.push(message);
-      convo.latestMessageText = message.text;
-      return convo;
+      const convoCopy = {...convo, id: message.conversationId, latestMessageText: message.text};
+      convoCopy.messages.push(message);
+      return convoCopy;
     } else {
       return convo;
     }
